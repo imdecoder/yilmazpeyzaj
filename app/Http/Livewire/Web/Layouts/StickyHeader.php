48fe559ff043090d1f48f859ced38950a0e8cd60
Wax <coderwax@gmail.com>
2022-06-06 00:18:19 +0300

    Web dosyaları güncellendi.

---
+++ app/Http/Livewire/Web/Layouts/StickyHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Web\Layouts;
+
+use Livewire\Component;
+
+class StickyHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.web.layouts.sticky-header');
+    }
+}
---
+++ resources/views/livewire/web/layouts/sticky-header.blade.php
@@ -0,0 +1,33 @@
+<!-- Sticky Header -->
+<div class="sticky-header">
+	<div class="auto-container clearfix">
+
+		<!-- Logo -->
+		<div class="logo pull-left">
+			<a href="{{ route('web.home') }}" title="Yılmaz Peyzaj">
+				<img src="{{ asset('assets/web/images/sticky-logo.png') }}" alt="Yılmaz Peyzaj" title="Yılmaz Peyzaj">
+			</a>
+		</div>
+
+		<!-- Right Col -->
+		<div class="pull-right clearfix">
+
+			<!-- Main Menu -->
+			<nav class="main-menu clearfix">
+				<!-- Keep This Empty / Menu will come through Javascript -->
+			</nav>
+			<!-- Main Menu End -->
+
+			<!-- Contact Btn -->
+			<div class="contact-link">
+				<a href="contact.html" class="theme-btn btn-style-three">
+					<span class="btn-title">
+						Teklif İste
+						<i class="arrow flaticon-play-button-1"></i>
+					</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End Sticky Menu -->

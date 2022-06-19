d49240eae0366a3744d7763f46d963d7c735d201
Wax <coderwax@gmail.com>
2022-06-19 16:03:04 +0300

    create: FooterBottom component

---
+++ app/Http/Livewire/Web/Layouts/FooterBottom.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Web\Layouts;
+
+use Livewire\Component;
+
+class FooterBottom extends Component
+{
+    public function render()
+    {
+        return view('livewire.web.layouts.footer-bottom');
+    }
+}
---
+++ resources/views/livewire/web/layouts/footer-bottom.blade.php
@@ -0,0 +1,17 @@
+<!-- Footer Bottom -->
+<div class="footer-bottom">
+	<div class="auto-container">
+		<div class="inner clearfix">
+			<div class="copyright">
+				&copy; Copyright {{ date('Y') }} Tüm Hakları Saklıdır.
+			</div>
+			<div class="bottom-links">
+				<ul class="clearfix">
+					<li><a href="#">Terms of Service</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+					<li><a href="#">Sitemap</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>

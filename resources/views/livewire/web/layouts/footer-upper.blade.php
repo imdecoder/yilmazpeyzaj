1ce6b3a156a148957e6bc70d0690082f30633007
Wax <coderwax@gmail.com>
2022-06-19 16:03:17 +0300

    create: FooterUpper component

---
+++ resources/views/livewire/web/layouts/footer-upper.blade.php
@@ -0,0 +1,111 @@
+<!-- Upper Section -->
+<div class="upper-section">
+	<div class="left-image"><img src="{{ asset('assets/web/images/resource/footer-left-image.png') }}" alt=""></div>
+	<div class="right-image"><img src="{{ asset('assets/web/images/resource/footer-right-image.png') }}" alt=""></div>
+
+	<div class="auto-container">
+
+		<div class="upper">
+			<div class="inner">
+				<div class="logo"><a href="#"><img src="{{ asset('assets/web/images/footer-logo.png') }}" alt=""></a></div>
+				<div class="clearfix">
+					<div class="text-box">
+						<div class="text">Pruners is established in the year of 2001 by Lee Rother , With operations throughout North America ... <a href="#" class="theme-btn">More About Us <i class="arrow flaticon-play-button-1"></i></a></div>
+					</div>
+					<div class="social">
+						<ul class="clearfix">
+							<li><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+							<li><a href="#"><span class="fab fa-twitter"></span></a></li>
+							<li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--Widgets-->
+		<div class="widgets-section">
+			<div class="row clearfix">
+
+				<!--Column-->
+				<div class="column col-xl-3 col-lg-4 col-md-12 col-sm-12">
+					<div class="footer-widget info-widget">
+						<div class="widget-title">
+							<h4>Get In Touch</h4>
+						</div>
+						<ul class="info">
+							<li class="address">PO Box 515381 <br>Pruners, Garden Street <br>LA 90029 USA</li>
+							<li class="timing">
+								Opening Hrs: <br>Mon-Sat: 9am to 7pm
+							</li>
+							<li>
+								<span class="phone"><a href="tel:+56789012345">(+5) 678 90 12 345</a></span><br>
+								<span class="email"><a href="mailto:service@Pruners.com">service@Pruners.com</a></span>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<!--Column-->
+				<div class="column col-xl-6 col-lg-4 col-md-12 col-sm-12">
+					<div class="footer-widget services-widget">
+						<div class="widget-title">
+							<h4>Main Services</h4>
+						</div>
+						<div class="widget-content">
+							<div class="row clearfix">
+								<div class="col-xl-6 col-lg-12 col-md-6 col-sm-12">
+									<ul>
+										<li><a href="#"><span class="icon flaticon-hedge"></span><span class="txt">Spring Cleanup</span><span class="sub-txt">Details <i class="arrow flaticon-play-button-1"></i></span></a></li>
+										<li><a href="#"><span class="icon flaticon-digging-1"></span><span class="txt">Plants Planting</span><span class="sub-txt">Details <i class="arrow flaticon-play-button-1"></i></span></a></li>
+										<li><a href="#"><span class="icon flaticon-sprinkler"></span><span class="txt">Water Fountain</span><span class="sub-txt">Details <i class="arrow flaticon-play-button-1"></i></span></a></li>
+									</ul>
+								</div>
+								<div class="col-xl-6 col-lg-12 col-md-6 col-sm-12">
+									<ul>
+										<li><a href="#"><span class="icon flaticon-painting"></span><span class="txt">Hardscaping</span><span class="sub-txt">Details <i class="arrow flaticon-play-button-1"></i></span></a></li>
+										<li><a href="#"><span class="icon flaticon-wheelbarrow"></span><span class="txt">Garden Care</span><span class="sub-txt">Details <i class="arrow flaticon-play-button-1"></i></span></a></li>
+										<li><a href="#"><span class="icon flaticon-terrain"></span><span class="txt">Soil Preparation</span><span class="sub-txt">Details <i class="arrow flaticon-play-button-1"></i></span></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!--Column-->
+				<div class="column col-xl-3 col-lg-4 col-md-12 col-sm-12">
+					<div class="footer-widget links-widget">
+						<div class="widget-title">
+							<h4>Quick Links</h4>
+						</div>
+						<div class="widget-content">
+							<ul class="links">
+								<li><a href="#">About Company</a></li>
+								<li><a href="#">Projects</a></li>
+								<li><a href="#">Testimonials</a></li>
+								<li><a href="#">News & Updates</a></li>
+								<li><a href="#">Working Process</a></li>
+								<li><a href="#">Contact Us</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<div class="footer-widget newsletter-widget">
+			<!--Newsletter-->
+			<div class="newsletter-form">
+				<form method="post" action="contact.html">
+					<div class="form-group clearfix">
+						<input type="text" name="name" value="" placeholder="Your Name *" required>
+						<input type="email" name="email" value="" placeholder="Email Address *" required>
+						<button type="submit" class="theme-btn btn-style-three"><span class="btn-title">Subscribe Us <i class="arrow flaticon-play-button-1"></i></span></button>
+					</div>
+				</form>
+			</div>
+		</div>
+
+	</div>
+</div>
